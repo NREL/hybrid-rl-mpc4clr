de6c4e8871bf82fba7f69e44a5a25154571e6222
aeseye <aeseye@nrel.gov>
2021-11-19 09:24:11 -0700

    adding new RL env classes and load restoration Pyomo OPF models

--- gym_opf_env/opf_envs/envs/__init__.py
+++ gym_opf_env/opf_envs/envs/__init__.py
@@ -6,6 +6,14 @@ from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv
 from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv2
 from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv3
 from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv4
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv5
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv6
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv7
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv8
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv10
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv11
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv12
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv13
 
 from opf_envs.envs.renewable_power_forecast_reader import ForecastReader
 
@@ -14,3 +22,5 @@ from opf_envs.envs.lr_opf_jump import OPFJuMP
 from opf_envs.envs.lr_opf_pyomo import OPFPyomo
 from opf_envs.envs.lr_opf_pyomo import OPFPyomo2
 from opf_envs.envs.lr_opf_pyomo import OPFPyomo3
+from opf_envs.envs.lr_opf_pyomo import OPFPyomo4
+from opf_envs.envs.lr_opf_pyomo import OPFPyomo5
