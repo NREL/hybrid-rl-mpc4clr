521114dd40eb44d0f9f61fd9a5d22174ec82ad24
aeseye <aeseye@nrel.gov>
2021-11-19 09:28:47 -0700

    adding the new envs for agent training

--- rl_learning/ray_train.py
+++ rl_learning/ray_train.py
@@ -8,10 +8,18 @@ from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv
 from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv2
 from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv3
 from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv4
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv5
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv6
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv7
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv8
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv10
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv11
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv12
+from opf_envs.envs.gym_reserve_policy import ReservePolicyEnv13
 
 current_file_path = os.getcwd()
 LOG_PATH = os.path.join(current_file_path, 'results/')  # Local
-#LOG_PATH = os.path.join('/scratch/aeseye/', 'rl_mpc_reserve_policy/')  # HPC
+#LOG_PATH = os.path.join('/scratch/aeseye/', 'rl_mpc_reserve_policy/results')  # HPC
 
 if __name__ == "__main__":
 
@@ -20,20 +28,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Training RLlib agents to learn power system reserves")
     parser.add_argument("--num-cpus", type=int, default=0)
     parser.add_argument("--num-gpus", type=int, default=0)
-    parser.add_argument("--name-env", type=str, default="ReservePolicy-v0")
+    parser.add_argument("--name-env", type=str, default="ReservePolicy-v6")
     parser.add_argument("--algo", type=str, default="PPO")
     parser.add_argument("--redis-password", type=str, default=None)
     args = parser.parse_args()
 
     ## Register the ReservePolicy RL environment
   
-    env_name = "ReservePolicyEnv-v0"
-    register_env(env_name, lambda config: ReservePolicyEnv())
+    env_name = "ReservePolicyEnv-v6"
+    register_env(env_name, lambda config: ReservePolicyEnv7())
 
     ## Initialize Ray
 
-    ray.init(_temp_dir="/tmp/scratch/ray")      # HPC
-    #ray.init(_node_ip_address='192.168.0.10')  # local, add your IP if NREL VPN is ON (during local running)
+    #ray.init(_temp_dir="/tmp/scratch/ray")      # HPC
+    ray.init(_node_ip_address='192.168.0.10')  # local, add your IP if NREL VPN is ON (during local running)
     num_cpus = args.num_cpus - 1
 
     ## Run TUNE Experiments (Train the RL Agent)
@@ -41,8 +49,8 @@ if __name__ == "__main__":
     tune.run(
         args.algo,
         name=env_name,     
-        stop={"time_total_s": 3600},  # or: stop={"training_iteration": 100}
-        checkpoint_freq=10,
+        stop={"time_total_s": 6*3600},  # or: stop={"training_iteration": 100}
+        checkpoint_freq=30,
         checkpoint_at_end=True,
         checkpoint_score_attr="episode_reward_mean",
         keep_checkpoints_num=3,
